765df5fa701b5909676951e101d48718a776125d
Tushar <rishiwalia2000@gmail.com>
2022-01-17 22:16:46 +0530

    displayed posts and user name who posted

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ console.log('router loaded');
 
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
+
 
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
