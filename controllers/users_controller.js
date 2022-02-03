15f4cbcb80191c59711f3b53a2623c0218670e39
Tushar <rishiwalia2000@gmail.com>
2022-02-03 17:19:15 +0530

    added profile picture upload feature using multer

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,35 +1,82 @@
 const User = require('../models/user');
+const fs = require('fs');
+const path = require('path');
 
+module.exports.profile = function (req, res) {
+    User.findById(req.params.id, function (err, user) {
 
-module.exports.profile = function(req, res){
-    User.findById(req.params.id, function(err,user){
-    
         return res.render('user_profile', {
             title: 'User Profile',
             profile_user: user
-    });
-    
+        });
+
     });
 }
 
-module.exports.update = function(req, res){
-    if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
-            
-            req.flash('success', 'User updated successfully');
+module.exports.update = async function (req, res) {
+    // if(req.user.id == req.params.id){
+    //     User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
 
-            return res.redirect('back');
-        });
-    }else{
+    //         req.flash('success', 'User updated successfully');
+
+    //         return res.redirect('back');
+    //     });
+    // }else{
+    //     return res.status(401).send('Unauthorized');
+    // }
+
+    if (req.user.id == req.params.id) {
+
+        try {
+
+            let user = await User.findByIdAndUpdate(req.params.id);
+            User.uploadedAvatar(req, res, function (err) {
+                if (err) { console.log('***Multer Error: ', err) }
+
+                //console.log(req.file);
+
+                user.name = req.body.name;
+                user.email = req.body.email;
+
+                if (req.file) {
+
+                    if(user.avatar){
+                        fs.unlinkSync(path.join(__dirname, '..' , user.avatar));  
+                    }
+
+
+                    //this is saving path of the uploaded file into the avatar field in the user
+
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+
+                user.save();
+
+                return res.redirect('back');
+
+            })
+
+        } catch {
+            req.flash('error', 'Post creation failed');
+
+            console.log('Error', err);
+            return;
+        }
+
+
+    } else {
+
         return res.status(401).send('Unauthorized');
+
     }
+
 }
 
 // render the sign up page
-module.exports.signUp = function(req, res){
+module.exports.signUp = function (req, res) {
 
     //if user already signed in redirect to profile page
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         return res.redirect('/users/profile');
     }
 
@@ -41,11 +88,11 @@ module.exports.signUp = function(req, res){
 
 
 // render the sign in page
-module.exports.signIn = function(req, res){
+module.exports.signIn = function (req, res) {
 
     //if user already signed in , redirect to profile page
-    if(req.isAuthenticated()){
-       return res.redirect('/users/profile');
+    if (req.isAuthenticated()) {
+        return res.redirect('/users/profile');
     }
 
     return res.render('user_sign_in', {
@@ -54,37 +101,50 @@ module.exports.signIn = function(req, res){
 }
 
 // get the sign up data
-module.exports.create = function(req, res){
-    if (req.body.password != req.body.confirm_password){
+module.exports.create = function (req, res) {
+    if (req.body.password != req.body.confirm_password) {
         return res.redirect('back');
     }
 
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('error in finding user in signing up'); return}
+    User.uploadedAvatar(req, res, function (err) {
 
-        if (!user){
-            User.create(req.body, function(err, user){
-                if(err){console.log('error in creating user while signing up'); return}
-
-                return res.redirect('/users/sign-in');
-            })
-        }else{
-            return res.redirect('back');
+        if (err) {
+            console.log('***Multer Error: ', err)
         }
 
-    });
+            User.findOne({ email: req.body.email }, function (err, user) {
+
+                if (err) { console.log('error in finding user in signing up'); }
+        
+                if (!user) {
+                    User.create(req.body, function (err, user) {
+                        if (err) { console.log('error in creating user while signing up', err); return }
+        
+                        if (req.file) {
+                            user.avatar = User.avatarPath + '/' + req.file.filename;
+                            user.save();
+                        }
+                            
+                        return res.redirect('/users/sign-in');
+                    })
+                } else {
+                    return res.redirect('back');
+                }
+        
+            });
+        });
 }
 
 
 // sign in and create a session for the user
-module.exports.createSession = function(req, res){
-   
+module.exports.createSession = function (req, res) {
+
     req.flash('success', 'Logged in Successfully');
 
     return res.redirect('/');
 }
 
-module.exports.destroySession = function(req,res){
+module.exports.destroySession = function (req, res) {
     req.logout();
 
     req.flash('success', 'You have logged out!');
--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -13,11 +16,30 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    avatar:{
+        type: String,
     }
 }, {
     timestamps: true
 });
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,'..',AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+
+  //static methods
+
+  //single is used so that the user can only upload one file
+  userSchema.statics.uploadedAvatar = multer({storage: storage}).single('avatar');
+  
+  //for accessing AVATAR_PATH globally
+  userSchema.statics.avatarPath = AVATAR_PATH;
 
 const User = mongoose.model('User', userSchema);
 
