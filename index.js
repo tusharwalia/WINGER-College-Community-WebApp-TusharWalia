af08f86eb67fda4f740d492cd43e71534c4e244d
Tushar <rishiwalia2000@gmail.com>
2022-02-10 16:51:25 +0530

    added google oauth feature at login and signup

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
+const passportGoogle = require('./config/passport-google-oauth2-strategy');
+
 const MongoStore = require('connect-mongodb-session')(session);
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
+
 
 
 app.use(sassMiddleware({
