07f4630894c9630f29ad52384ef747a8c66cf3e1
Tushar <rishiwalia2000@gmail.com>
2022-05-01 10:39:33 +0530

    fixing socket issue in chat_engine.js

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -3,7 +3,7 @@ class ChatEngine{
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
 
-        this.socket = io.connect('http://localhost:5000');
+        this.socket = io.connect('http://44.201.130.207:5000');
 
         if (this.userEmail){
             this.connectionHandler();
