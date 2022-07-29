022ec0ac3c7458ea9e7320870f4ff7644bf37b2d
Tushar <rishiwalia2000@gmail.com>
2022-07-29 18:20:48 +0530

    .

--- config/environment.js
+++ config/environment.js
@@ -21,12 +21,12 @@ const development ={
         port: 587,
         secure: true,
         auth: {
-            user: 'rishicr72000',
-            pass: 'cocorishi@123'
+            user: 'username',
+            pass: 'password'
         }
     },
-    google_client_id: '459761749362-k5d5rgfmr7fh2p4iq197s7jcpnk6dif1.apps.googleusercontent.com', // e.g. asdfghjkkadhajsghjk.apps.googleusercontent.com
-    google_client_secret: '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', // e.g. _ASDFA%KFJWIASDFASD#FAD-
+    google_client_id: '', // e.g. asdfghjkkadhajsghjk.apps.googleusercontent.com
+    google_client_secret: '', // e.g. _ASDFA%KFJWIASDFASD#FAD-
     google_callback_url: "http://localhost:8000/users/auth/google/callback",
     jwt_secret: 'winger' ,
     morgan: {
@@ -35,32 +35,32 @@ const development ={
     }
 }
 
-const production = {
-    name: 'production',
-    asset_path: process.env.WINGER_ASSET_PATH,
-    session_cookie_key: process.env.WINGER_SESSION_COOKIE_KEY,
-    db: 'winger_production',
-    smtp: {
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.WINGER_GMAIL_USERNAME,
-            pass: process.env.WINGER_GMAIL_PASSWORD
-        }
-    },
-    google_client_id: process.env.WINGER_GOOGLE_CLIENT_ID, // e.g. asdfghjkkadhajsghjk.apps.googleusercontent.com
-    google_client_secret: process.env.WINGER_GOOGLE_CLIENT_SECRET , // e.g. _ASDFA%KFJWIASDFASD#FAD-
-    google_callback_url: process.env.WINGER_GOOGLE_CLIENT_CALLBACK_URL ,
-    jwt_secret: process.env.WINGER_JWT_SECRET,
-    morgan: {
-        mode: 'combined',
-        options: {stream: accessLogStream}
-    }
+// const production = {
+//     name: 'production',
+//     asset_path: process.env.WINGER_ASSET_PATH,
+//     session_cookie_key: process.env.WINGER_SESSION_COOKIE_KEY,
+//     db: 'winger_production',
+//     smtp: {
+//         service: 'gmail',
+//         host: 'smtp.gmail.com',
+//         port: 587,
+//         secure: false,
+//         auth: {
+//             user: process.env.WINGER_GMAIL_USERNAME,
+//             pass: process.env.WINGER_GMAIL_PASSWORD
+//         }
+//     },
+//     google_client_id: process.env.WINGER_GOOGLE_CLIENT_ID, // e.g. asdfghjkkadhajsghjk.apps.googleusercontent.com
+//     google_client_secret: process.env.WINGER_GOOGLE_CLIENT_SECRET , // e.g. _ASDFA%KFJWIASDFASD#FAD-
+//     google_callback_url: process.env.WINGER_GOOGLE_CLIENT_CALLBACK_URL ,
+//     jwt_secret: process.env.WINGER_JWT_SECRET,
+//     morgan: {
+//         mode: 'combined',
+//         options: {stream: accessLogStream}
+//     }
 
-}
+// }
 
-//module.exports = eval(process.env.WINGER_ENVIRONMENT) == undefined ? development : eval(process.env.WINGER_ENVIRONMENT) ;
+// //module.exports = eval(process.env.WINGER_ENVIRONMENT) == undefined ? development : eval(process.env.WINGER_ENVIRONMENT) ;
 
 module.exports = development;
--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -6,7 +6,7 @@ exports.newComment = (comment) => {
     let htmlString = nodeMailer.renderTemplate({comment: comment}, './comments/new_comment.ejs')
 
     nodeMailer.transporter.sendMail({
-        from: 'rishicr72000@gmail.com',
+        from: 'your email',
         to: comment.user.email,
         subject: "New commnet published",
         html: htmlString
